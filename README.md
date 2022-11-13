f70efd71956f8941b06afdf8b6732254cf3ed1ef
Dominik Brack <dominik.brack@gmail.com>
2022-11-13 16:31:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# React Date Timeline
+
+A simple timeline component to build things such as roadmaps based on a date range.
+Run `npm install` and `npm start` to start the dev server locally.
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+export function App() {
+	return (
+		<div className="App">
+			<Button variant="contained">Hello World</Button>
+		</div>
+	)
+}
